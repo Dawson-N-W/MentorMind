3459ec85c2ecf204937060b829df1e7f72fd96d5
Dawson-N-W <Dawson.n.williams@gmail.com>
2023-03-21 10:33:27 -0700

    Added "application" package for Main Added comments of requirements

---
+++ src/Application/Main.java
@@ -0,0 +1,13 @@
+package Application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+//login in for the first time
+//login with non-default password
+//resetting password
+//logging out
+//write initial data to SQLLite DB for items 13 to 18 in problem statement
+//data entry for items 7.1 to 7.8 of problem statement (saving is optional)
