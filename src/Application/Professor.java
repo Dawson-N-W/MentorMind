bc4c682a68a9c17ebda3023def7948877c53a209
Dawson-N-W <Dawson.n.williams@gmail.com>
2023-03-22 14:34:44 -0700

    Professor stuff

--- src/Application/Professor.java
+++ src/Application/Professor.java
@@ -1,33 +1,47 @@
 package Application;
 
 public class Professor implements Uni_Member{
+    //instance variables
+    private String firstName;
+    private String lastName;
+
+    private String school;
+    DAL dal;
+    //constructor
+    public Professor(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        dal = new DAL();
+    }
+    //default constructor
+    public Professor(){};
     @Override
     public void setFirstName(String name) {
-
+        this.firstName = name;
     }
 
     @Override
     public String getLastName() {
-        return null;
+        return lastName;
     }
 
     @Override
     public void setLastName(String name) {
-
+        this.lastName = name;
     }
 
     @Override
     public String getFirstName() {
-        return null;
+        return firstName;
     }
 
     @Override
     public void setSchool(String school) {
-
+        this.school = school;
     }
 
     @Override
     public String getSchool() {
-        return null;
+        return school;
     }
 }
