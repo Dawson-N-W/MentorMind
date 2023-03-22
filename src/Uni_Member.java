bc5eaf3dec7520e3c58095b55800a759ad62a648
Dawson-N-W <Dawson.n.williams@gmail.com>
2023-03-22 13:42:44 -0700

    Added Student, Prof, Uni_Member Interface

---
+++ src/Uni_Member.java
@@ -0,0 +1,10 @@
+public interface SJSU_Member {
+    public void setFirstName(String name);
+    public String getLastName();
+
+    public void setLastName(String name);
+    public String getFirstName();
+
+    public void setSch
+
+}
